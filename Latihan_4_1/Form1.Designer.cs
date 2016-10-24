a587622acf1f759da04de39c915dd5616275f575
KelvinChen1996 <kelvin_chen96@ymail.com>
2016-10-24 17:36:01 +0700

    commit 2

---
+++ Latihan_4_1/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace Latihan_4_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripButton_Bold = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton_Italic = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton_Underline = new System.Windows.Forms.ToolStripButton();
+            this.toolStripComboBox2 = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripComboBox3 = new System.Windows.Forms.ToolStripComboBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.toolStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 92);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(425, 136);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.SelectionChanged += new System.EventHandler(this.richTextBox1_SelectionChanged);
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripComboBox1,
+            this.toolStripButton_Bold,
+            this.toolStripButton_Italic,
+            this.toolStripButton_Underline,
+            this.toolStripComboBox2,
+            this.toolStripComboBox3});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(466, 25);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripComboBox1
+            // 
+            this.toolStripComboBox1.Name = "toolStripComboBox1";
+            this.toolStripComboBox1.Size = new System.Drawing.Size(121, 25);
+            this.toolStripComboBox1.SelectedIndexChanged += new System.EventHandler(this.toolStripComboBox1_SelectedIndexChanged);
+            // 
+            // toolStripButton_Bold
+            // 
+            this.toolStripButton_Bold.CheckOnClick = true;
+            this.toolStripButton_Bold.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton_Bold.Image = global::Latihan_4_1.Properties.Resources.B;
+            this.toolStripButton_Bold.Name = "toolStripButton_Bold";
+            this.toolStripButton_Bold.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton_Bold.Click += new System.EventHandler(this.toolStripButton_Bold_Click);
+            // 
+            // toolStripButton_Italic
+            // 
+            this.toolStripButton_Italic.CheckOnClick = true;
+            this.toolStripButton_Italic.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton_Italic.Image = global::Latihan_4_1.Properties.Resources.i;
+            this.toolStripButton_Italic.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton_Italic.Name = "toolStripButton_Italic";
+            this.toolStripButton_Italic.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton_Italic.Text = "I";
+            this.toolStripButton_Italic.Click += new System.EventHandler(this.toolStripButton_Italic_Click);
+            // 
+            // toolStripButton_Underline
+            // 
+            this.toolStripButton_Underline.CheckOnClick = true;
+            this.toolStripButton_Underline.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton_Underline.Image = global::Latihan_4_1.Properties.Resources.u;
+            this.toolStripButton_Underline.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton_Underline.Name = "toolStripButton_Underline";
+            this.toolStripButton_Underline.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton_Underline.Text = "toolStripButton3";
+            this.toolStripButton_Underline.Click += new System.EventHandler(this.toolStripButton_Underline_Click);
+            // 
+            // toolStripComboBox2
+            // 
+            this.toolStripComboBox2.Name = "toolStripComboBox2";
+            this.toolStripComboBox2.Size = new System.Drawing.Size(121, 25);
+            this.toolStripComboBox2.SelectedIndexChanged += new System.EventHandler(this.toolStripComboBox2_SelectedIndexChanged);
+            // 
+            // toolStripComboBox3
+            // 
+            this.toolStripComboBox3.Name = "toolStripComboBox3";
+            this.toolStripComboBox3.Size = new System.Drawing.Size(121, 25);
+            this.toolStripComboBox3.SelectedIndexChanged += new System.EventHandler(this.toolStripComboBox3_SelectedIndexChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(466, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.ShortcutKeyDisplayString = "";
+            this.newToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.newToolStripMenuItem.Text = "&New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "&Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Text = "&Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(466, 261);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
+        private System.Windows.Forms.ToolStripButton toolStripButton_Bold;
+        private System.Windows.Forms.ToolStripButton toolStripButton_Italic;
+        private System.Windows.Forms.ToolStripButton toolStripButton_Underline;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox2;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+
+    }
+}
+
---
+++ Latihan_4_1/Form1.cs
@@ -0,0 +1,335 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+
+namespace Latihan_4_1
+{
+    public partial class Form1 : Form
+    {
+        public bool isSave = true;
+        public string direktoriSave = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void refresh_teks()
+        {
+
+            float fontsize;
+            if (toolStripComboBox1.Text == "")
+                fontsize = 12;
+            else
+                fontsize = (float)Convert.ToDouble(toolStripComboBox1.SelectedItem);
+
+            FontStyle style = (toolStripButton_Bold.Checked) ? FontStyle.Bold : FontStyle.Regular;
+            style |= (toolStripButton_Italic.Checked) ? FontStyle.Italic : FontStyle.Regular;
+            style |= (toolStripButton_Underline.Checked) ? FontStyle.Underline : FontStyle.Regular;
+
+            Font font_baru = new Font(toolStripComboBox2.SelectedItem.ToString(), fontsize, style);
+
+            richTextBox1.SelectionFont = font_baru;
+            richTextBox1.SelectionColor = Color.FromName(toolStripComboBox3.Text);
+            richTextBox1.Focus();
+        }
+
+        private void save()
+        {
+            DialogResult dialog_result;
+            if (direktoriSave == "")
+            {
+                saveFileDialog1.Filter = "Rich Text Format (*.rtf)|*.rtf";
+                dialog_result = saveFileDialog1.ShowDialog();
+                if (dialog_result == DialogResult.OK)
+                {
+                    richTextBox1.SaveFile(saveFileDialog1.FileName);
+                    direktoriSave = saveFileDialog1.FileName;
+                }
+            }
+            else
+            {
+                richTextBox1.SaveFile(direktoriSave);
+            }
+            isSave = true;
+        }
+
+        private void exit()
+        {
+            if (!isSave)
+            {
+                DialogResult dialog_result;
+                dialog_result = MessageBox.Show("Apakah Anda ingin menyimpan file terlebih dahulu?", "Simpan file", MessageBoxButtons.YesNo);
+                if (dialog_result == DialogResult.No)
+                {
+                    Application.ExitThread();
+                }
+                else if (dialog_result == DialogResult.Yes)
+                {
+                    save();
+                    Application.ExitThread();
+                }
+            }
+            else
+                Application.ExitThread();
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                save();
+            }
+            catch (Exception ec)
+            {
+                MessageBox.Show(ec.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InstalledFontCollection kumpulanfont = new InstalledFontCollection();
+
+            // menempatkan nama2 font family pada combobox2
+            foreach (FontFamily i in kumpulanfont.Families)
+            {
+                toolStripComboBox2.Items.Add(i.Name);
+            }
+            toolStripComboBox2.SelectedIndex = 9; // Arial default
+
+
+            //menciptakan ukuran font mulai dari 5 hingga 72 pada combobox 1
+            for (int i = 5; i <= 72; i++)
+                toolStripComboBox1.Items.Add(i);
+            toolStripComboBox1.SelectedIndex = 7;  // 12
+
+            toolStripComboBox2.ComboBox.LostFocus += new EventHandler(toolStripComboBox2_LostFocus);
+            toolStripComboBox1.ComboBox.LostFocus += new EventHandler(toolStripComboBox2_LostFocus);
+            toolStripComboBox2.ComboBox.DrawItem += new DrawItemEventHandler(toolStripComboBox2_DrawItem);
+            toolStripComboBox2.ComboBox.DrawMode = DrawMode.OwnerDrawFixed;
+
+
+            //mengambil semua properties warna dan disimpan ke combobox 3
+            foreach (PropertyInfo property in typeof(Color).GetProperties())
+            {
+                if (property.PropertyType == typeof(Color))
+                    toolStripComboBox3.Items.Add(property.Name);
+            }
+
+            toolStripComboBox3.SelectedIndex = 8;// Black default
+            toolStripComboBox3.ComboBox.LostFocus += new EventHandler(toolStripComboBox2_LostFocus);
+            toolStripComboBox3.ComboBox.DrawItem += new DrawItemEventHandler(toolStripComboBox3_DrawItem);
+            toolStripComboBox3.ComboBox.DrawMode = DrawMode.OwnerDrawFixed;
+
+            refresh_teks();
+        }
+
+        private void toolStripComboBox2_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            //menampilkan font style agar kelihatan
+            e.DrawBackground();
+            e.Graphics.DrawString(toolStripComboBox2.Items[e.Index].ToString(), new Font(toolStripComboBox2.Items[e.Index].ToString(), toolStripComboBox2.Font.Size), Brushes.Black, e.Bounds);
+
+        }
+        private void toolStripComboBox3_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            // mengatasi agar dropdownlist kelihatan 
+
+
+            if (e.Index >= 0)
+            {
+                Graphics g = e.Graphics;
+                Brush brush = new SolidBrush(e.BackColor);
+                Brush tBrush = new SolidBrush(e.ForeColor);
+
+                g.FillRectangle(brush, e.Bounds);
+                string s = (string)this.toolStripComboBox3.Items[e.Index].ToString();
+                SolidBrush b = new SolidBrush(Color.FromName(s));
+                //Menggambar persegi panjang dan mengisinya dengan warna saat ini
+                //dan menambahkan nama di sebelah kanan warna
+                e.Graphics.DrawRectangle(Pens.Black, 2, e.Bounds.Top + 1, 20, 11);
+                e.Graphics.FillRectangle(b, 3, e.Bounds.Top + 2, 19, 10);
+                e.Graphics.DrawString(s, this.Font, Brushes.Black, 25, e.Bounds.Top);
+                brush.Dispose();
+                tBrush.Dispose();
+            }
+            e.DrawFocusRectangle();
+
+        }
+
+        private void toolStripComboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ToolStripComboBox TSC = (ToolStripComboBox)sender;
+            if (!TSC.Focused)
+                return;
+            refresh_teks();
+        }
+
+        private void toolStripComboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ToolStripComboBox TSC = (ToolStripComboBox)sender;
+            if (!TSC.Focused)
+                return;
+            refresh_teks();
+        }
+
+        private void toolStripComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ToolStripComboBox TSC = (ToolStripComboBox)sender;
+            if (!TSC.Focused)
+                return;
+            refresh_teks();
+        }
+
+        private void toolStripButton_Italic_Click(object sender, EventArgs e)
+        {
+            refresh_teks();
+        }
+
+        private void toolStripButton_Underline_Click(object sender, EventArgs e)
+        {
+            refresh_teks();
+        }
+
+        private void toolStripButton_Bold_Click(object sender, EventArgs e)
+        {
+            refresh_teks();
+        }
+
+        private void richTextBox1_SelectionChanged(object sender, EventArgs e)
+        {
+            if (richTextBox1.SelectionFont != null)
+            {
+                try
+                {
+                    toolStripComboBox1.SelectedIndex = (int)richTextBox1.SelectionFont.Size - 5;
+                }
+                catch
+                {
+                    toolStripComboBox1.Text = "";
+                }
+
+                try
+                {
+                    toolStripComboBox2.SelectedItem = richTextBox1.SelectionFont.FontFamily.Name;
+                }
+                catch
+                {
+                    toolStripComboBox2.Text = "";
+                }
+
+                try
+                {
+                    toolStripComboBox3.SelectedItem = richTextBox1.SelectionColor.Name;
+                }
+                catch
+                {
+                    toolStripComboBox3.Text = "";
+                }
+
+                if (richTextBox1.SelectionFont.Style.ToString().IndexOf("Bold") != -1)
+                    toolStripButton_Bold.Checked = true;
+
+                if (richTextBox1.SelectionFont.Style.ToString().IndexOf("Italic") != -1)
+                    toolStripButton_Italic.Checked = true;
+
+                if (richTextBox1.SelectionFont.Style.ToString().IndexOf("Underline") != -1)
+                    toolStripButton_Underline.Checked = true;
+            }
+            else
+            {
+                toolStripComboBox1.SelectedIndex = 7;
+                toolStripComboBox2.SelectedIndex = 9;
+            }
+        }
+
+        private void toolStripComboBox2_LostFocus(object sender, EventArgs e)
+        {
+            refresh_teks();
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult dialog_result;
+                if (!isSave)
+                {
+                    dialog_result = MessageBox.Show("Apakah Anda ingin menyimpan file ini?", "Save file", MessageBoxButtons.YesNoCancel,MessageBoxIcon.Information);
+                    if (dialog_result == DialogResult.Cancel)
+                    {
+                        return;
+                    }
+                    else if (dialog_result == DialogResult.Yes)
+                    {
+                        save();
+                    }
+                }
+
+                richTextBox1.Clear();
+                direktoriSave = "";
+                isSave = false;
+            }
+            catch (Exception ec)
+            {
+                MessageBox.Show(ec.Message);
+            }
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult dialog_result;
+                if (!isSave)
+                {
+                    dialog_result = MessageBox.Show("Apakah Anda ingin menyimpan file ini?", "Save file", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Information);
+                    
+                    if (dialog_result == DialogResult.Yes)
+                    {
+                        save();
+                    }
+                }
+                dialog_result = openFileDialog1.ShowDialog();
+                if (dialog_result == DialogResult.OK)
+                {
+                    direktoriSave = openFileDialog1.FileName;
+                    isSave = true;
+                    richTextBox1.LoadFile(openFileDialog1.FileName);
+                }
+            }
+            catch (Exception ec)
+            {
+                MessageBox.Show(ec.Message);
+            }
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            isSave = false;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            exit();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult dialog_result = MessageBox.Show("Apakah Anda ingin keluar?", "Keluar file", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+            if(dialog_result==DialogResult.Yes)
+                exit();
+
+        }
+
+    }
+}
